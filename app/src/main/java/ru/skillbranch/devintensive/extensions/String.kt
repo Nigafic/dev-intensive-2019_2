54fe741905b33eec3be2ec871f3ec6bea31ab550
Nigafic <1302035246ghost@gmail.com>
2019-08-23 20:44:23 +0300

    Update String.kt

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -10,7 +10,6 @@ fun String.truncate(count: Int = 16): String {
     }
 }
 
-// заменяем все множественные пробелы на одинарный и удаляем все html-теги + html-escape последовательности
 fun String.stripHtml(): String = replace( "<.*?>|&amp;|&gt;|&lt;|&quot;|&apos;|&nbsp;".toRegex(), "")
     .replace("\\s+".toRegex(), " ")
 
